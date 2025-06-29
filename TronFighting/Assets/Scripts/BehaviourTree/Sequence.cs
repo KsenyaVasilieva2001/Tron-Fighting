fce270b1edeca54d03332de79e8b55cbab05613e
Ksenia Vasilieva <ksenya_vasilieva@mail.ru>
2025-06-29 17:16:53 +0300

    Add sequence

--- TronFighting/Assets/Scripts/BehaviourTree/Sequence.cs
+++ TronFighting/Assets/Scripts/BehaviourTree/Sequence.cs
@@ -1,18 +1,38 @@
+using BehaviorTree;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sequence : MonoBehaviour
+public class Sequence : Node
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sequence() : base() { }
+    public Sequence(List<Node> children) : base(children) { }
+
+    public override NodeState Evaluate()
     {
-        
+        bool anyChildIsRunning = false;
+
+        foreach (Node node in children)
+        {
+            switch (node.Evaluate())
+            {
+                case NodeState.FAILURE:
+                    state = NodeState.FAILURE;
+                    return state;
+                case NodeState.SUCCESS:
+                    continue;
+                case NodeState.RUNNING:
+                    anyChildIsRunning = true;
+                    continue;
+                default:
+                    state = NodeState.SUCCESS;
+                    return state;
+            }
+        }
+
+        state = anyChildIsRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+        return state;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
