128085f9fced153b91d2125d87177b529cc73ab9
Ksenia Vasilieva <ksenya_vasilieva@mail.ru>
2025-07-03 00:22:37 +0300

    Add rotation

--- TronFighting/Assets/Scripts/PlayerControllers/PlayerController.cs
+++ TronFighting/Assets/Scripts/PlayerControllers/PlayerController.cs
@@ -6,31 +6,52 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public UIController uiController;
-    public Enemy currentEnemy;
+    public EnemyBT currentEnemy;
+    public bool IsInFightZone;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        GlobalEvents.OnPlayerEnterFightZone += PlayFightAnim;
-        GlobalEvents.OnPlayerExitFightZone += PlayIdleAnim;
+        GlobalEvents.OnPlayerEnterFightZone += HandleEnterFightZone;
+        GlobalEvents.OnPlayerExitFightZone += HandleExitFightZone;
     }
 
-    public void PlayFightAnim(EnemyBT enemy)
+    private void Update()
+    {
+        if (IsInFightZone) RotateTowards(currentEnemy);
+    }
+
+    public void HandleEnterFightZone(EnemyBT enemy)
     {
         animator.Play("fight_idle");
+        IsInFightZone = true;
+        currentEnemy = enemy;
     }
 
-    public void PlayIdleAnim(EnemyBT enemy)
+    public void HandleExitFightZone(EnemyBT enemy)
     {
         animator.Play("idle");
+        IsInFightZone = false;
+    }
+
+    protected void RotateTowards(EnemyBT enemy)
+    {
+        var direction = (enemy.transform.position - transform.position).normalized;
+        direction.y = 0f;
+        if (direction.sqrMagnitude < 0.0001f)
+            return;
+        Quaternion targetRot = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, 720f * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        /*
         if (other.TryGetComponent(out Enemy enemy))
         {
             currentEnemy = enemy;
             uiController.SetEnemyUI(enemy.data);
         }
+        */
     }
 }
